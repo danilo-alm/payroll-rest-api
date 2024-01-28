db24bc2b5c52dd087652541cc04aa7db57c4baf1
Danilo Almeida <cs.daniloalmeida@gmail.com>
2024-01-28 14:17:58 -0300

    feat: add Employee

---
+++ src/main/java/com/danilo/payroll/domain/employee/Employee.java
@@ -0,0 +1,21 @@
+package com.danilo.payroll.domain.employee;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@RequiredArgsConstructor
+public class Employee {
+    private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) long id;
+    private @NonNull String name;
+    private @NonNull String role;
+}
