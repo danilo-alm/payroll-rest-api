9e32a885fa25e50eb9e70060487d925fa84fbed9
Danilo Almeida <cs.daniloalmeida@gmail.com>
2024-01-28 14:22:10 -0300

    feat: preload database

---
+++ src/main/java/com/danilo/payroll/infra/LoadDatabase.java
@@ -0,0 +1,23 @@
+package com.danilo.payroll.infra;
+
+import com.danilo.payroll.domain.employee.Employee;
+import com.danilo.payroll.repositories.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class LoadDatabase {
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+        };
+    }
+}
